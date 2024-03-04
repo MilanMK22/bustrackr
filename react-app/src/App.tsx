58398c5568e54ed676ab928886e413599894ebbe
Milan Kriletich <kriletmila@ecs.vuw.ac.nz>
2024-03-04 20:21:28 +1300

    starting react front end

---
+++ react-app/src/App.tsx
@@ -0,0 +1,6 @@
+import SearchBar from './SearchBar' ;
+
+function App() {
+  return <div><SearchBar></SearchBar></div>
+}
+export default App
---
+++ react-app/src/SearchBar.tsx
@@ -0,0 +1,6 @@
+
+function SearchBar(){
+    return<h1> SearchBar</h1>;
+}
+
+export default SearchBar
