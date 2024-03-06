3da048d8fb9d264880a1281d102aed93a91bc459
Milan Kriletich <kriletmila@aunty-mena.ecs.vuw.ac.nz>
2024-03-06 15:56:04 +1300

    displaying bus number and time to arrival

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -3,23 +3,23 @@ import SearchBar from "./components/SearchBar";
 import { fetchBusStops } from "./services/busStopService";
 import ListGroup from "./components/ListGroup";
 
-
 interface BusStop {
   busNumber: string;
   estimatedArrivalTime: string | null;
   timeDifference: number | null;
 }
 
-
 function App() {
-
   const [busStops, setBusStops] = useState<BusStop[]>([]);
+  const [key, setKey] = useState<number>(0); // Unique key to force component remount
 
   const handleSearch = async (searchValue: string) => {
     try {
       const response: any = await fetchBusStops(searchValue); // Explicitly declare response type as any
-      if (response) { // Check if response and response.data exist
+      if (response) {
+        // Check if response and response.data exist
         setBusStops(response);
+        setKey((prevKey) => prevKey + 1);
       } else {
         console.error("Invalid response format:", response);
       }
@@ -28,9 +28,14 @@ function App() {
     }
   };
 
+  useEffect(() => {
+    console.log(busStops);
+  }, [busStops]); // Log busStops whenever it changes
+
   return (
     <div>
       <SearchBar onSearch={handleSearch}></SearchBar>
+      <ListGroup key={key} items={busStops} heading="buses"></ListGroup>
     </div>
   );
 }
--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,27 @@
+interface BusStop {
+  busNumber: string;
+  estimatedArrivalTime: string | null;
+  timeDifference: number | null;
+}
 
-function ListGroup(){
+interface Props {
+  items: BusStop[];
+  heading: string;
+}
 
-
-
-
-
-    return <h1>List</h1>;
-    
+function ListGroup({ items, heading }: Props) {
+  const message = items.length === 0 ? <p>No Item Found</p> : null;
+  return (
+    <ul className="list-group">
+      {message}
+      {items.map((item) => (
+        <li className="list-group-item" key={item.busNumber}>
+          <strong>Bus Number:</strong> {item.busNumber} &nbsp;&nbsp;&nbsp;&nbsp;
+          <strong>Time to Arrival:</strong> {item.timeDifference} minutes
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default ListGroup;
