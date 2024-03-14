3ee5ce0891b31ba1b27e6aa105bb3c0e24cbf36d
Milan Kriletich <kriletmila@ecs.vuw.ac.nz>
2024-03-15 11:51:57 +1300

    changing back to using deployed service

--- react-app/src/services/busStopService.tsx
+++ react-app/src/services/busStopService.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export async function fetchBusStops(searchValue: string): Promise<any[]> {
   try {
     const response = await axios.get<any[]>(
-      `http://localhost:3000/api/bus-stop/${searchValue}`
+      `https://bustrackr-e09i.onrender.com/api/bus-stop/${searchValue}`
     );
     return response.data;
   } catch (error) {
