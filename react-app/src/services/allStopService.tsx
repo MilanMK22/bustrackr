f304822ef97314ebe7df5822180be2a4ccccd791
Milan Kriletich <kriletmila@ecs.vuw.ac.nz>
2024-03-15 21:27:44 +1300

    fixing return value in all stop service

--- react-app/src/services/allStopService.tsx
+++ react-app/src/services/allStopService.tsx
@@ -5,9 +5,7 @@ export async function fetchAllStopIds(): Promise<string[]> {
     const response = await axios.get<any[]>(
       "https://bus-trackr.netlify.app/.netlify/functions/allStops"
     );
-    const stopIds: string[] = response.data.map((stop: any) => stop.stop_id);
-    console.log(stopIds);
-    return stopIds;
+    return response.data;
   } catch (error) {
     console.error("Error fetching all stop IDs:", error);
     throw error; // Re-throw the error to be handled by the caller
